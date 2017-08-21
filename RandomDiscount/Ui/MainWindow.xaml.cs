6b58bf5836a344266af0101807ab40fd5ab5982c
dron247 <bigdigdgs@gmail.com>
2017-08-21 18:40:05 +0800

    updated design

--- RandomDiscount/Ui/MainWindow.xaml.cs
+++ RandomDiscount/Ui/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RandomDiscount {
     /// <summary>
@@ -25,6 +26,7 @@ namespace RandomDiscount {
         /// </summary>
         enum State {
             Game, // the screen with a cat
+            Progress, // progress screen
             Results // the screen with a results
         }
 
@@ -51,7 +53,7 @@ namespace RandomDiscount {
         private void ButtonGame_Click(object sender, RoutedEventArgs e) {
             EnterState( // we select to which state we have to move the screen
                 currentState == State.Game ?
-                State.Results :
+                State.Progress :
                 State.Game
             );
         }
@@ -72,15 +74,30 @@ namespace RandomDiscount {
                     MediaBlock.Visibility = Visibility.Visible;
                     Discount.Visibility = Visibility.Collapsed;
                     ButtonText.Text = Properties.Resources.ButtonGame;
+                    progress.Visibility = Visibility.Hidden;
+                    break;
+                case State.Progress:
+                    progress.Visibility = Visibility.Visible;
+                    new DispatcherTimer().Let(timer => {
+                        timer.Interval = new TimeSpan(0, 0, 2);
+                        timer.Tick += (sender, e) => {
+                            timer.Stop();
+                            EnterState(State.Results);
+                        };
+                        timer.Start();
+                    });
+
                     break;
                 case State.Results: // calculater results and show result items
                     Discount.Text = $"{randomizerService.Result}%"; // our discount
                     MediaBlock.Visibility = Visibility.Collapsed;
                     Discount.Visibility = Visibility.Visible;
                     ButtonText.Text = Properties.Resources.ButtonAgain;
+                    progress.Visibility = Visibility.Hidden;
                     break;
             }
             currentState = newState;
         }
+     
     }
 }
